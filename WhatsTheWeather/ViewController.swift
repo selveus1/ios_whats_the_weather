89a5b9079fa6a3a1106bf6b7171a728c4453eea8
selveus1 <fs031090@gmail.com>
2018-02-17 08:05:59 -0500

    Initial Commit

---
+++ WhatsTheWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsTheWeather
+//
+//  Created by Athena on 2/17/18.
+//  Copyright © 2018 Sheena Elveus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
